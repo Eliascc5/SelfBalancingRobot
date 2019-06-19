cb2807039290c27e779ca1e8bfc41ced18a55608
eliasmck <elias.coko@gmail.com>
2019-06-19 11:17:14 -0300

    Comunicacion I2C - para lectura de MPU6050

---
+++ SelfBalancingRobot/lib/src/i2c_DEV.h
@@ -0,0 +1,11 @@
+
+#ifndef DEV_ADDR
+	#define DEV_ADDR 0x68
+#endif
+#include <stdint.h>
+uint8_t DEV_rdy(uint8_t ad0);
+uint8_t DEV_write(uint8_t ad0, uint8_t address, uint8_t dato);
+uint8_t DEV_write_N(uint8_t ad0, uint8_t address, uint8_t *data);
+uint8_t DEV_read(uint8_t ad0, uint8_t address);
+uint16_t DEV_read16(uint8_t ad0, uint8_t address); //lee entero 16 bits;
+uint8_t DEV_read_fast(uint8_t ad0);

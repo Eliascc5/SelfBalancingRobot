9ffefb119e9d3b75cf21ecc3ba44c5dca0aad50a
Jeremias Pino <jerepino@gmail.com>
2019-06-20 20:04:15 -0300

    resuelvo conflicto

--- SelfBalancingRobot/test/timer_imple.c
+++ SelfBalancingRobot/test/timer_imple.c
@@ -2,25 +2,35 @@
 #include <util/delay.h>
 #include <time.h>
 #include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <util/delay.h>
+ISR(TIMER0_COMPA_vect){
+  _delay_ms(100);
+  PORTD ^= (1<<PORTD4);
+  TIFR0 &=~ (1<<OCF0A); //apaga flag
+}
 int main(int argc, char const *argv[]) {
-  DDRB |= (1<<DDB1) | (1<<DDB2) | (1<<DDB0);
-  confModoSalidas_T8(2, 2);
-  interrupciones_T8(0,0,0);
-  confPrescaler_T8(2000);
+  DDRD |= (1<<PORTD6) | (1<<PORTD4);
+  // PORTD |= (1<<PORTD4);
   confModo_T8(2);
-  // conf_PWM_14_TC1(1000, 100, 900);
+  confModoSalidas_T8(1, 0);
+  interrupciones_T8(1,0,0);
+  confPrescaler_T8(10);
+
+  // // conf_PWM_14_TC1(1000, 100, 900);
+  setDutyA8(10);
   sei();
-  srand(time(NULL));
-
-  setDutyB8(900);
+  // setDutyB8(900);
+  // TCCR0A |= (1<<WGM01);
+  // TCCR0A |= (1<<COM0A0);
+  // TCCR0B |= (5<<CS00);
+  // OCR0A = (uint8_t)(255); //Numero hasta el cual cuenta para llegar al
+  // sOLO PUEDO CONTAR HASTA 256 -> LAS FRECUENCIAS SON MAS CHICAS.
   while (1) {
-      for (size_t i = 0; i < 2001; i+=100) {
-        setDutyA8(i);
-        _delay_ms(4000);
-        PORTB ^= (1<<PORTB0);
-      }
-
-      _delay_ms(1000);
+      _delay_ms(1);
   }
 
   return 0;

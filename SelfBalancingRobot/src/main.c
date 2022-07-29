661965f8a3dda542671b6ab988cd1780c113115e
Eliascc5 <elias.coko@gmail.com>
2022-07-29 16:39:21 +0200

    reorganizing

--- SelfBalancingRobot/src/main.c
+++ SelfBalancingRobot/src/main.c
@@ -13,21 +13,9 @@
 * Materiales: ATMEGA328p, MPU6050, 2 DC Motors, L298N
 * Autores: Correa Elias y Pino Jeremias
 */
-#include <stdint.h>
-#include <stdio.h>
-#include <math.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-#include <avr/io.h>
-/*Drivers*/
-#include "timer_uno.h"
-#include "i2c_master.h"
-#include "mpu_6050.h"
-#include "i2c_DEV.h"
-#include "UART_ATmega0.h"
-#include "pid.h"
-#include "gpio_register_atmega328p.h"
-#include "processing_data.h"
+#include"main.h"
+
+
 /*USART*/
 #define UART_BAUDRATE 57600
 #define DEV_ADDR 0x68			// Direccion estandar de MPU
@@ -77,57 +65,7 @@ ISR(TIMER1_COMPA_vect){
     TIFR1 &=~ (1<<OCF1A); //apaga flag
 }
 
-void InterpretaComando(void){
-	int aux_i;
-  double aux_f;
-	switch(comando[0]){
-    case 'S': //SETPOINT
-          aux_i = atoi(&comando[1]); // lee desde el caracter 1 hasta el caracter nulo y lo convierte en int
-          if (aux_i <= ANG_MAX && aux_i >= ANG_MIN){
-              SETPOINT = aux_i;
-          }else{
-          printf("ST muy grande:%d\n",SETPOINT);
-          }
-          break;
-		case 'P':
-		   aux_f = atof(&comando[1]); // string to float
-       setKP(aux_f);
-		   break;
-    case 'I':
-		   aux_f = atof(&comando[1]); // string to float
-       setKI(aux_f);
-		   break;
-    case 'D':
- 			 aux_f = atof(&comando[1]); // string to float
-       setKD(aux_f);
- 			 break;
-    case 'F':
-      SETPOINT = 85.0; //Esto hay que sacarlo de aca y setearlo en el interpreta comando
-      estado_s = Move_forward;
-      break;
-    case 'B':
-      SETPOINT = 95.0; //Esto hay que sacarlo de aca y setearlo en el interpreta comando
-      estado_s = Move_backward;
-      break;
-    case 'R':
-      estado_s = Turn_right;
-      break;
-    case 'L':
-      estado_s = Turn_left;
-      break;
-    case 'A':
-      estado_s = Retain_immobile;
-      break;
-    case 'O':
-      estado_s = Stop;
-      break;
-    case 'E': //Consulta estado. Para hacerlo mas detallado se podria hacer switch estado y printear el nombre en lugar del numero
-      printf("Estado %d\n", estado_s);
-      break;
-    default:
-      break;
-  }
-}
+
 
 /*Interrupcion recepcion uart*/
 ISR(USART_RX_vect){
